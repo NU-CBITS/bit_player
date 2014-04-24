4d03a27a9e7aebc981bc5bac2a3574998449c983
Eric Carty-Fickes <ericcf@northwestern.edu>
2014-04-24 11:57:52 -0500

    added helper methods and bumped version

--- app/models/bit_player/tool.rb
+++ app/models/bit_player/tool.rb
@@ -9,5 +9,15 @@ module BitPlayer
     validates :title, presence: true
     validates :position, uniqueness: true
     validates :is_home, inclusion: { in: [true, false] }
+
+    def add_module(title)
+      content_modules.create(title: title, position: last_position + 1)
+    end
+
+    private
+
+    def last_position
+      content_modules.order(:position).last.try(:position) || 0
+    end
   end
 end
