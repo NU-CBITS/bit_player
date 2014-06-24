ea6d01944c56832f9e5befeb93d91d181b4ef9e1
Eric Carty-Fickes <ericcf@northwestern.edu>
2014-06-24 11:48:36 -0500

    fix migration bugs

--- db/migrate/20140620182756_convert_slides_slideshows_to_bit_core.rb
+++ db/migrate/20140620182756_convert_slides_slideshows_to_bit_core.rb
@@ -1,5 +1,6 @@
 class BitPlayer::Slideshow < ActiveRecord::Base; end
 class BitPlayer::Slide < ActiveRecord::Base; end
+class BitPlayer::VideoSlide < BitPlayer::Slide; end
 
 class ConvertSlidesSlideshowsToBitCore < ActiveRecord::Migration
   def up
@@ -7,20 +8,21 @@ class ConvertSlidesSlideshowsToBitCore < ActiveRecord::Migration
       BitCore::Slideshow.create!(title: slideshow.title, id: slideshow.id)
     end
     BitPlayer::Slide.all.each do |slide|
+      type = (slide.type || "").gsub(/BitPlayer::VideoSlide/, "BitCore::VideoSlide")
       BitCore::Slide.create!(
         title: slide.title,
         id: slide.id,
         body: slide.body,
         position: slide.position,
-        bit_core_slideshow_id: bit_player_slideshow_id,
-        type: slide.type,
+        bit_core_slideshow_id: slide.bit_player_slideshow_id,
+        type: type,
         options: slide.options,
         is_title_visible: slide.is_title_visible
       )
     end
     execute <<-SQL
       ALTER TABLE bit_player_slides
-        DROP CONSTRAINT fk_slides_slideshows
+        DROP CONSTRAINT IF EXISTS fk_slides_slideshows
     SQL
 
     execute <<-SQL
